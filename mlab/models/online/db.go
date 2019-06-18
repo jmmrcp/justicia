0740bb62cdd02b1bb05973050dfc201367f5f883
Joe Martin <jmmrcpsip@gmail.com>
2019-06-18 19:17:13 +0200

    AL dia

--- mlab/models/online/db.go
+++ mlab/models/online/db.go
@@ -19,8 +19,8 @@ type (
 func NewDB() (*DB, error) {
 	ctx := context.Background()
 	// uri := "mongodb://jmmrcp:J538MTUSbg3v3Vh@ds263876.mlab.com:63876/justicia"
-	uri := "mongodb://u2mnmnnqwwiu6tr0ugs9:4uYFMYouheqLOLnCeipb@bhmydkfk8yl7h0i-mongodb.services.clever-cloud.com:27017/bhmydkfk8yl7h0i"
-	// uri := "mongodb+srv://jmmrcpsip:SK0umjgZr0qxTS3b@justice-sbfoj.mongodb.net/test?retryWrites=true&w=majority"
+	// uri := "mongodb://u2mnmnnqwwiu6tr0ugs9:4uYFMYouheqLOLnCeipb@bhmydkfk8yl7h0i-mongodb.services.clever-cloud.com:27017/bhmydkfk8yl7h0i"
+	uri := "mongodb+srv://jmmrcpsip:!Niergsf5!aJkWH@justice-sbfoj.mongodb.net/test?retryWrites=true&w=majority"
 	client, err := mongo.NewClient(
 		options.Client().ApplyURI(uri),
 	)
@@ -30,7 +30,7 @@ func NewDB() (*DB, error) {
 	if err := client.Connect(ctx); err != nil {
 		return nil, err
 	}
-	// db := client.Database("justicia")
-	db := client.Database("bhmydkfk8yl7h0i")
+	db := client.Database("justicia")
+	//db := client.Database("bhmydkfk8yl7h0i")
 	return &DB{db, ctx}, nil
 }
