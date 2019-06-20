74414e85ad03d7e80863005d35d55de9dfcf2881
jmmrcp <jmmrcp@gmail.com>
2019-06-20 08:58:12 +0200

    Studio 3T Conexion

--- quiz/config/config.go
+++ quiz/config/config.go
@@ -32,7 +32,8 @@ func GetMlabDB() (*mongo.Database, error) {
 func GetMongoDB() (*mongo.Database, error) {
 	ctx := context.Background()
 	client, err := mongo.NewClient(
-		options.Client().ApplyURI("mongodb+srv://jmmrcpsip:SK0umjgZr0qxTS3b@justice-sbfoj.mongodb.net/test?retryWrites=true&w=majority"),
+		options.Client().ApplyURI("mongodb+srv://jmmrcpsip:SK0umjgZr0qxTS3b@justice-sbfoj.mongodb.net/admin?replicaSet=Justice-shard-0&connectTimeoutMS=10000&authSource=admin&authMechanism=SCRAM-SHA-1&3t.uriVersion=3&3t.connection.name=Justice-shard-0&3t.databases=admin,test"),
+		//options.Client().ApplyURI("mongodb+srv://jmmrcpsip:SK0umjgZr0qxTS3b@justice-sbfoj.mongodb.net/test?retryWrites=true&w=majority"),
 	)
 	if err != nil {
 		return nil, err
